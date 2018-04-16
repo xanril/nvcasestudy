5500098eba9c29f910f22d8288f46eee3fdd236f
xanril <motionsmith.ph@gmail.com>
2018-04-17 04:33:52 +0800

    cleanup of unused references and added DataAnnotiations

--- CaseStudy/Screens/AddFlightScreen.cs
+++ CaseStudy/Screens/AddFlightScreen.cs
@@ -1,9 +1,7 @@
 ﻿using CaseStudy.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace CaseStudy.Screens
 {
