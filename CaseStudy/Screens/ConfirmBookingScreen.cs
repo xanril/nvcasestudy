7c29099e024d561fbe477e5f4000665a61d50a07
Kristian Mitra <xanril@users.noreply.github.com>
2018-04-18 15:14:07 +0800

    added new screen ConfirmationBookingScreen

---
+++ CaseStudy/Screens/ConfirmBookingScreen.cs
@@ -0,0 +1,67 @@
+﻿using CaseStudy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaseStudy.Screens
+{
+    class ConfirmBookingScreen : IScreen
+    {
+        private const string KEY_CONFIRM_BOOKING = "Y";
+        private const string KEY_CANCEL_BOOKING = "N";
+
+        private Reservation reservation;
+
+        public ConfirmBookingScreen(Reservation reservation)
+        {
+            this.reservation = reservation;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("RESERVATIONS > BOOKING SUMMARY");
+            Console.WriteLine(reservation.GetInfo());
+            Console.WriteLine("");
+        }
+
+        public void ShowInputPrompt()
+        {
+            Console.Write("Confirm this booking? (Y/N): ");
+        }
+
+        public void ProcessInput(string userInput)
+        {
+            userInput = userInput.ToUpper();
+
+            switch(userInput)
+            {
+                case KEY_CONFIRM_BOOKING:
+
+                    try
+                    {
+                        DataManager.GetInstance().AddReservation(reservation);
+                        Console.WriteLine("Booking saved.");
+                        ScreenManager.GetInstance().PopScreenUntilTargetType(typeof(ReservationsScreen));
+                    }
+                    catch(Exception ex)
+                    {
+                        Console.WriteLine("There was an error creating a booking. Please try again.");
+                    }
+                    break;
+
+                case KEY_CANCEL_BOOKING:
+
+                    Console.WriteLine("Booking cancelled.");
+                    ScreenManager.GetInstance().PopScreenUntilTargetType(typeof(ReservationsScreen));
+                    break;
+
+                default:
+
+                    Console.WriteLine("Cannot recognize input. Please try again.");
+                    break;
+            }
+        }
+    }
+}
