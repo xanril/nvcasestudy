0313c78a898ed3a73d0c1f3954d1dfe485458e9b
xanril <motionsmith.ph@gmail.com>
2018-04-20 02:28:13 +0800

    created abstract definitions of presenter and view

---
+++ CaseStudy/Abstracts/AbstractPresenter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaseStudy.Abstracts
+{
+    abstract public class AbstractPresenter
+    {
+        protected AbstractView view;
+
+        //abstract public void ProcessInput(string userInput);
+    }
+}
---
+++ CaseStudy/Abstracts/AbstractView.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaseStudy.Abstracts
+{
+    abstract public class AbstractView
+    {
+        private string errorMessage;
+
+        abstract public void Display();
+        public abstract void ReadInput(string userInput);
+
+        public virtual void ShowError()
+        {
+            Console.WriteLine(errorMessage);
+            errorMessage = string.Empty;
+        }
+
+        public virtual void ShowInputPrompt()
+        {
+            if (string.IsNullOrEmpty(errorMessage) == false)
+                ShowError();
+        }
+
+        
+
+        public void SetErrorMessage(string errorMessage)
+        {
+            this.errorMessage = errorMessage;
+        }
+
+        public void ShowInputFeedback(string feedbackMessage)
+        {
+            Console.WriteLine(feedbackMessage);
+        }
+    }
+}
